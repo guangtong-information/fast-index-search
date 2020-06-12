d72a086eafbf54b5df487cc589a7fb707957884a
winlone <worm128@163.com>
2020-06-12 15:23:11 +0800

    提交页面

--- fis-index-web/src/test/java/DoubleCheck.java
+++ fis-index-web/src/test/java/DoubleCheck.java
@@ -13,7 +13,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class DoubleCheck {
     private static String data = "";
     private Lock lock = new ReentrantLock();// 锁对象
-    
+
 
     /**
      * double check: 处理并发的线程锁
@@ -21,7 +21,7 @@ public class DoubleCheck {
      * @return
      */
     public String doSomeThing(String name) {
-        System.out.println("进入时间:" + Comm.getData() + ",线程执行：" + name);
+        System.out.println("1、进入时间:" + Comm.getData() + ",线程执行：" + name);
         String returnData = "";
 
         try {
@@ -29,42 +29,42 @@ public class DoubleCheck {
             if (data == "") {
                 //线程锁，锁住1秒
                 if (lock.tryLock(3, TimeUnit.SECONDS)) {
-                    System.out.println("获取锁的时间:" +  Comm.getData() + ",获取锁：" + name);
+                    System.out.println("2、" + name + "获取锁，获取锁的时间:" + Comm.getData());
 
                     //第二次check:判断缓存是否为空，为空则进行缓存加载
                     if (data == "") {
                         int sec = 2000;
                         //读取数据 -> 制作缓存 耗费的时间
                         Thread.sleep(sec);
-                        System.out.println("制作缓存时间:" +  Comm.getData() + ",等待" + sec + "ms完毕：" + name);
+                        System.out.println("3、" + name + "制作缓存，当前时间:" + Comm.getData() + ",制作缓存时间" + sec + "ms完毕");
                         returnData = name + "做的缓存-数据库读取完成";
                         //赋值缓存
                         data = returnData;
 
                         lock.unlock();
-                        System.out.println("解锁时间:" +  Comm.getData() + ",解锁的线程：" + name);
+                        System.out.println("4、解锁的线程：" + name + ",解锁时间:" + Comm.getData());
                     } else {
                         //使用缓存
                         returnData = data;
-                        System.out.println("存在缓存时间:" +  Comm.getData() + ",有缓存：" + name);
+                        System.out.println("3、" + name + "读取到有缓存,当前时间:" + Comm.getData());
                     }
                 } else {
                     //使用缓存
                     returnData = data;
-                    System.out.println("存在缓存时间:" +  Comm.getData() + ",超时：" + name);
+                    System.out.println("3、" + name + "锁超时,存在缓存时间:" + Comm.getData());
                 }
             } else {
                 //使用缓存
                 returnData = data;
-                System.out.println("获取缓存时间:" +  Comm.getData() + ",有缓存：" + name);
+                System.out.println("3、" + name + "读取到有缓存,获取缓存时间:" + Comm.getData());
             }
 
         } catch (Exception e) {
-            System.out.println("线程错误：" + e.getMessage());
+            System.out.println("error：" + name + "线程错误：" + e.getMessage());
         } finally {
 
         }
-        System.out.println("离开时间:" +  Comm.getData() + ",线程结束：" + name);
+        System.out.println("end：" + name + "线程结束，离开时间:" + Comm.getData());
         return returnData;
     }
 
@@ -74,7 +74,7 @@ public class DoubleCheck {
         Thread a1 = new Thread() {
             public void run() {
                 String cache = doSomeThing("线程1");
-                System.out.println("读取1：" + cache);
+                System.out.println("线程1读取缓存数据为：" + cache);
             }
         };
         a1.setName("a1:" + System.currentTimeMillis());
@@ -85,7 +85,7 @@ public class DoubleCheck {
         Thread a2 = new Thread() {
             public void run() {
                 String cache = doSomeThing("线程2");
-                System.out.println("读取2：" + cache);
+                System.out.println("线程2读取缓存数据为：" + cache);
             }
         };
         a2.setName("a2:" + System.currentTimeMillis());
@@ -98,22 +98,22 @@ public class DoubleCheck {
         Thread a3 = new Thread() {
             public void run() {
                 String cache = doSomeThing("线程3");
-                System.out.println("读取3：" + cache);
+                System.out.println("线程3读取缓存数据为：" + cache);
             }
         };
         a3.setName("a3:" + System.currentTimeMillis());
         a3.start();
 
 
-        a1.join();
-        a2.join();
-        a3.join();
+//        a1.join();
+//        a2.join();
+//        a3.join();
         //永久等待主线程
-//        try {
-//            Thread.sleep(50000000);
-//        } catch (Exception e) {
-//        } finally {
-//        }
+        try {
+            Thread.sleep(50000000);
+        } catch (Exception e) {
+        } finally {
+        }
     }
 
 }
